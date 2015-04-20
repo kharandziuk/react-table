8da409425b5d3a0a293a542a45d17f91319be9e1
Max Kharandziuk <max.kharandziuk@gmail.com>
2015-04-20 15:38:03 +0300

    add form component

--- main.js
+++ main.js
@@ -1,7 +1,56 @@
 var React = require('react');
-console.log(React)
+var D = React.DOM;
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return D.form(
+      { onSubmit: alert},
+      D.div(
+        {
+          className: 'form-inline',
+        },
+        null,
+        D.div(
+            {className: 'form-group'},
+            D.label(null, 'First Name'),
+            D.input({className: "form-control"})
+        ),
+        D.div(
+            {className: 'form-group'},
+            D.label(null, 'Last Name'),
+            D.input({className: "form-control"})
+        )
+      ),
+      D.div(
+          {className: 'form-group'},
+          D.label(null, 'Phone'),
+          D.input({className: "form-control"})
+      ),
+      D.button({type: 'submit', className: 'btn btn-default'}, 'Submit')
+    );
+  }
+}
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return D.div(
+          {className: 'container'},
+          D.div(
+            {className: 'col-lg-offset-3'},
+            React.createElement(Form),
+            D.table({className: 'table'})
+          )
+      );
+    }
+}
 
 React.render(
-    React.DOM.div(null, 'hello'),
+    React.createElement(App),
     document.body
 );
